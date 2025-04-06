ef1531ae61f94e1ec4c26f9d8af3c853a87c2a45
aswath0001 <rsaswaths38@gmail.com>
2025-04-06 22:55:00 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./sections/Header.jsx";
+import Hero from "./sections/Hero.jsx";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+    </main>
+  );
+};
+
+export default App;
