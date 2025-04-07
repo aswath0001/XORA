6ee163cdcbdbc8a37887adcae339c7b8ab701e20
aswath0001 <rsaswaths38@gmail.com>
2025-04-07 23:32:44 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+import "./App.css";
 import Header from "./sections/Header.jsx";
 import Hero from "./sections/Hero.jsx";
-
-import "./App.css";
+import Features from "./sections/Features.jsx";
+import Pricing from "./sections/Pricing.jsx";
 
 const App = () => {
   return (
     <main className="overflow-hidden">
       <Header />
       <Hero />
+      <Features />
+      <Pricing />
     </main>
   );
 };
